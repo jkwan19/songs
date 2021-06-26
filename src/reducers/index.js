46d75e9b4d7102f122c778fa1e1aaddf02792522
Justin Kwan <jkwan19@gmail.com>
2021-06-26 14:24:51 -0700

    Combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
     { title: 'No Scrubs', Duration: '4:05' },
@@ -10,3 +12,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
   if (action.type === 'SONG_SELECTED') return action.payload;
   return selectedSong;
 }
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+})
